57d03091f6f1b8cb32bbcb3973d92c875b67be96
DRJungOok <DRJungOok@gmail.com>
2025-05-16 23:00:57 +0900

    edit

---
+++ src/javaAlgorhythm/baekjoon/BJ_1002.java
@@ -0,0 +1,14 @@
+package javaAlgorhythm.baekjoon;
+
+import java.util.Scanner;
+
+public class BJ_1002 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println(a-b);
+	}
+}
